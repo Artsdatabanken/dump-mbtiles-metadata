2cb88ccee42e8248d5bb399b8f5710bf77bc2f6e
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-09-12 17:47:54 +0200

    Fix: handling of paths when not using . as argument

--- dump-mbtiles-metadata.js
+++ dump-mbtiles-metadata.js
@@ -1,31 +1,33 @@
 #!/usr/bin/env node
-const sqlite = require("better-sqlite3");
-const fs = require("fs");
-var path = require("path");
+const sqlite = require("better-sqlite3")
+const fs = require("fs")
+var path = require("path")
 
 function readMetadata(mbtilesPath) {
-  const db = new sqlite(mbtilesPath);
-  const rows = db.prepare("SELECT * FROM metadata").all();
-  return rows.reduce((acc, row) => (acc[row.name] = row.value), {});
+  const db = new sqlite(mbtilesPath)
+  const rows = db.prepare("SELECT * FROM metadata").all()
+  return rows.reduce((acc, row) => (acc[row.name] = row.value), {})
 }
 
 function dumpAllMetadata(mbtilesPath) {
   const meta = fs
     .readdirSync(mbtilesPath)
-    .filter(
-      file => fs.statSync(file).isFile() && path.extname(file) === ".mbtiles"
-    )
-    .reduce(
-      (acc, file) => (acc[path.parse(file).name] = readMetadata(file)),
-      {}
-    );
-  console.log(JSON.stringify(meta));
+    .map(file => path.join(mbtilesPath, file))
+    .filter(file => {
+      console.log(JSON.stringify(path.parse(file)))
+      return fs.statSync(file).isFile() && path.extname(file) === ".mbtiles"
+    })
+    .reduce((acc, file) => {
+      acc[path.parse(file).name] = readMetadata(file)
+      return acc
+    }, {})
+  console.log(JSON.stringify(meta))
 }
 
 try {
-  if (process.argv.length === 3) dumpAllMetadata(process.argv[2]);
-  else throw new Error("Usage: node scan-mbtiles.js <directory>");
+  if (process.argv.length === 3) dumpAllMetadata(process.argv[2])
+  else throw new Error("Usage: node scan-mbtiles.js <directory>")
 } catch (error) {
-  console.error(error.message);
-  process.exit(1);
+  console.error(error)
+  process.exit(1)
 }
