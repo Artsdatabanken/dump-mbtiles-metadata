31dbba3bb301f194062773d625a97a74a27c8179
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-01-16 14:22:43 +0100

    Log all files in directory structure
    Extended meta info only for mbtiles

--- dump-mbtiles-metadata.js
+++ dump-mbtiles-metadata.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
-const sqlite = require("better-sqlite3")
-const fs = require("fs")
-var path = require("path")
+const sqlite = require("better-sqlite3");
+const fs = require("fs");
+var path = require("path");
 
 function readMetadata(mbtilesPath) {
   try {
-    const db = new sqlite(mbtilesPath)
-    const rows = db.prepare("SELECT * FROM metadata").all()
+    const db = new sqlite(mbtilesPath);
+    const rows = db.prepare("SELECT * FROM metadata").all();
     return rows.reduce((acc, row) => {
-      acc[row.name] = row.value
-      return acc
-    }, {})
+      acc[row.name] = row.value;
+      return acc;
+    }, {});
   } catch (error) {
-    console.error(`Error reading ${mbtilesPath}: ${error.message}`)
-    return { error: error.message }
+    console.error(`Error reading ${mbtilesPath}: ${error.message}`);
+    return { error: error.message };
   }
 }
 
@@ -23,20 +23,23 @@ function dumpAllMetadata(basePath, mbtilesPath, acc = {}) {
     .map(file => path.join(mbtilesPath, file))
     .reduce((acc, file) => {
       if (fs.statSync(file).isDirectory()) {
-        dumpAllMetadata(basePath, file, acc)
-      } else if (path.extname(file) === ".mbtiles")
-        acc[path.relative(basePath, file)] = readMetadata(file)
-      return acc
-    }, acc)
+        dumpAllMetadata(basePath, file, acc);
+        return;
+      }
+      const ext = path.extname(file);
+      acc[path.relative(basePath, file)] =
+        ext === ".mbtiles" ? readMetadata(file) : {};
+      return acc;
+    }, acc);
 }
 
 try {
   if (process.argv.length === 3) {
-    const sourcePath = process.argv[2]
-    const meta = dumpAllMetadata(sourcePath, sourcePath)
-    console.log(JSON.stringify(meta))
-  } else throw new Error("Usage: node scan-mbtiles.js <directory>")
+    const sourcePath = process.argv[2];
+    const meta = dumpAllMetadata(sourcePath, sourcePath);
+    console.log(JSON.stringify(meta));
+  } else throw new Error("Usage: node dump-mbtiles-metadata.js <directory>");
 } catch (error) {
-  console.error(error)
-  process.exit(1)
+  console.error(error);
+  process.exit(1);
 }
